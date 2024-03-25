2ad55afe1d83871362a0605e0e8ae3218a01c848
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-03-25 10:32:50 +0000

    mvp2 ish added header

---
+++ react-flashcards/src/assets/Header/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+	return (
+		<header>
+			<img src="#" alt="" />
+			<h1>Flashcards</h1>
+			<p>Expand your React knowledge, one flashcard at a time.</p>
+		</header>
+	);
+}
