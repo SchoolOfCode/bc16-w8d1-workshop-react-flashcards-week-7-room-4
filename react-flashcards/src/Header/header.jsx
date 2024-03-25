a1d617caebeb83fa1431b9f6de3736a4dfc2889e
mrw46 <mrw46@outlook.com>
2024-03-25 12:00:08 +0000

    MVP 2 finished

--- react-flashcards/src/Header/header.jsx
+++ react-flashcards/src/Header/header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
 	return (
-		<header>
+		<header id="header">
 			<img src="#" alt="" />
 			<h1>Flashcards</h1>
 			<p>Expand your React knowledge, one flashcard at a time.</p>
--- react-flashcards/src/Main/main.jsx
+++ react-flashcards/src/Main/main.jsx
@@ -3,9 +3,9 @@ import FlashcardList from "./FlashcardList/flashcardlist"
 
 export default function Main(){
     return(
-        <>
+        <div className="main">
             <Form/>
             <FlashcardList/>
-        </>
+        </div>
     )
 }
